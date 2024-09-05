6b5af34b727a0cfa5906ffd20aa8d0ca87195a83
byeonsanghwa <sh.byeon20@gmail.com>
2024-09-05 13:13:13 +0900

    filter added

--- src/main/java/com/example/account/configuration/LogFilter.java
+++ src/main/java/com/example/account/configuration/LogFilter.java
@@ -7,12 +7,14 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Slf4j
-@Component
 public class LogFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse
             , FilterChain filterChain) throws IOException, ServletException {
-        // 외부 -> filter
-
+        // 외부 -> filter( -> 처리) filter -> 외부
+        log.info("Hello LogFilter"+Thread.currentThread());
+        filterChain.doFilter(servletRequest,servletResponse);
+        log.info("Bye LogFilter"+Thread.currentThread());
     }
 }
+
---
+++ src/main/java/com/example/account/configuration/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.account.configuration;
+
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    FilterRegistrationBean<Filter> LoggingFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean =
+                new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
